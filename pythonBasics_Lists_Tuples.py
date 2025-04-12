779d338bc2dc0793d6666b2ee3af18961627a4c8
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2025-04-12 19:05:40 +0530

    basic array and tuples

---
+++ pythonBasics_Lists_Tuples.py
@@ -0,0 +1,20 @@
+# Lists basically array ki tarah hai.
+lisT=[1,2,3,"sarthak"]
+
+print(lisT)
+print(len(lisT))
+# lisT.sort(), print(lisT) it won't work cause string and integer dono data type hai ek hi mai
+
+list2=[1,2,3,444,555,-10001,-2,2,4,6566,1,8,7,6,4,3]
+list2.sort()
+print(list2) # it works only integers are there, basically same data type hona chahiye sorting ke liye
+
+
+
+# ------------------------------------------------------------------------------------------------
+
+# Tuples basically immutable hote hai, no addition, deletion or anything can be done here
+
+tupple=(1,1,1,23,4,0,-00,1)
+print(tupple)
+print(type(tupple))
