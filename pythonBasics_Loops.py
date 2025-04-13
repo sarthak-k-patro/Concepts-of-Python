abc0bbe0f0af3852bdecf5e667cc9f6d1ef9a5e6
Sarthak Kumar Patro <patrosarthak3@gmail.com>
2025-04-13 14:54:55 +0530

    WHile loop and printing elements using a loop in a List n Tuple

---
+++ pythonBasics_Loops.py
@@ -0,0 +1,13 @@
+# #  While Loop
+# count=1
+# while count<=5:
+#     print("count is :",count)
+#     count += 1
+
+#  Printing list elements using a loop
+# lisT=["sarthak","patro",1,7,8,900]
+lisT=("sarthak","patro",1,7,8,88,1,1,1,1,1,1,1,900)
+i=0
+while i<len(lisT):
+    print(lisT[i])
+    i=i+1
